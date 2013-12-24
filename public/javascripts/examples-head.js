02d97c98777aed2e6f0035e5d28cf6a44ca17e14
idarkfox <2486950@qq.com>
2013-12-24 18:17:52 +0800

    new my blog

---
+++ public/javascripts/examples-head.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sw on 13-12-23.
+ */
+document.writeln('\<link type="text\/css" rel="stylesheet" href="\/stylesheets\/SyntaxHighlighter.css"><\/link>');
+
+document.writeln('\<script language="javascript" src="\/javascripts\/syntaxhighlighter\/shCore.js"><\/script>');
+document.writeln('\<script language="javascript" src="\/javascripts\/syntaxhighlighter\/shBrushJScript.js"><\/script>');
+document.writeln('\<script language="javascript" src="\/javascripts\/syntaxhighlighter\/shBrushXml.js"><\/script>');
