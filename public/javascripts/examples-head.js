405fcf3c3d77a03d25a60461fc560fc3efc1380b
idarkfox <idarkfox@qq.com>
2022-07-02 02:03:35 +0800

    Delete public directory

--- public/javascripts/examples-head.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by sw on 13-12-23.
- */
-document.writeln('\<link type="text\/css" rel="stylesheet" href="\/public\/stylesheets\/SyntaxHighlighter.css"><\/link>');
-
-document.writeln('\<script language="javascript" src="\/public\/javascripts\/syntaxhighlighter\/shCore.js"><\/script>');
-document.writeln('\<script language="javascript" src="\/public\/javascripts\/syntaxhighlighter\/shBrushJScript.js"><\/script>');
-document.writeln('\<script language="javascript" src="\/public\/javascripts\/syntaxhighlighter\/shBrushXml.js"><\/script>');
